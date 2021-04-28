41caba168ff0ab98be816d9c2eeb1130683260a3
sajaalraja <sajamajed611@yahoo.com>
2021-04-29 00:22:53 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-"use strict"
-let name1 =prompt('please enter first name' );
+ let name1=prompt('please enter first name' );
 let name2=prompt('please enter last name ' );
-let result = name1+name2;
+let result = name1+name2;"use strict"
+
 console.log(result);
+
+
+let color1=prompt('please enter color1' );
+let color2=prompt('please enter color2 ' );
+let result1 = color1+color2;
+
+console.log(result1);
+
+
+let country=prompt('please enter country' );
+let country2=prompt('please enter another country ' );
+let result3 = country+country2;
+
+console.log(result3);
+
+
+let name3=prompt('please enter your namee' );
+let job=prompt('please enter job ' );
+let result4 = name3+job;
+
+console.log(result4);
+
